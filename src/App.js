57151cd64dab4ef53ffb25d198fccc62261cafce
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-04-30 19:30:02 +0300

    Added material-ui carousel, working on it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import Header from './layout/Header';
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <Landing></Landing>
     </Fragment>
   );
 }
