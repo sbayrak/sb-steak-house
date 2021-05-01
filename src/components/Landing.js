ebab10f78ade3f55267c886aec6beb2b7467a9a1
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-05-01 12:31:30 +0300

    Landing page is done along with responsive design

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,113 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@material-ui/core';
+import { Paper, Button, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxHeight: '95vh',
+    objectFit: 'cover',
+  },
+  gridItem: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    borderRadius: '20px',
+    width: '300px',
+    marginRight: theme.spacing(2),
+    opacity: '0.8',
+    marginBottom: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+      width: '175px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '165px',
+    },
+  },
+  Typo: {
+    color: '#fff',
+  },
+  Typo1: {
+    width: '70%',
+    fontFamily: 'Quicksand, sans-serif',
+    fontWeight: theme.typography.fontWeightBold,
+    paddingBottom: theme.spacing(2),
+    color: theme.palette.grey[300],
+    letterSpacing: '3px',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '60px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '30px',
+    },
+  },
+  Typo2: {
+    width: '70%',
+    paddingBottom: theme.spacing(7),
+    color: theme.palette.grey[300],
+    fontWeight: theme.typography.fontWeightMedium,
+    fontSize: '16px',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '16px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '13px',
+    },
+  },
+  btn: {
+    border: '1px solid #ff3c3c',
+    padding: theme.spacing(1),
+    paddingRight: theme.spacing(6),
+    paddingLeft: theme.spacing(6),
+    fontSize: '16px',
+    transition: '0.3s ease',
+    textDecoration: 'none',
+    borderRadius: '5px',
+    '&:hover': {
+      border: `1px solid ${theme.palette.grey[200]}`,
+    },
+  },
+}));
 
 function Landing(props) {
   var items = [
     {
-      name: 'Random Name #1',
-      description: 'Probably the most random thing you have ever seen!',
+      img: '/images/steak1.png',
+      typo1: 'STEAK, RIB-EYE, T-BONE',
+      typo2:
+        'Our meats are getting cooked for 12 hours with walnut and oak woods to get that flavour.',
+      button: 'MORE',
     },
     {
-      name: 'Random Name #2',
-      description: 'Hello World!',
+      img: '/images/steak2.png',
+      typo1: 'WOOD, FIRE, CHARCOAL',
+      typo2:
+        'In BAYRAK Steakhouse, you will not feel a different taste than the BBQ you make at home  ',
+      button: 'CHEFS',
+    },
+    {
+      img: '/images/steak3.png',
+      typo1: 'QUALITY, SATISFACTION, TASTE',
+      typo2:
+        'Since 1995, the good quality meal comes from good quality meat. That`s why we are so careful about choosing the right meat.',
+      button: 'MENU',
     },
   ];
 
   return (
-    <Carousel interval='3000'>
+    <Carousel interval='3500' indicators='true' navButtonsAlwaysVisible='true'>
       {items.map((item, i) => (
         <Item key={i} item={item} />
       ))}
@@ -23,14 +115,50 @@ function Landing(props) {
   );
 }
 
-function Item(props) {
+function Item({ item }) {
+  const classes = useStyles();
   return (
-    <Paper>
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
+    <div className={classes.root}>
+      <img src={item.img} alt={item.img}></img>
 
-      <Button className='CheckButton'>Check it out!</Button>
-    </Paper>
+      <Grid container className={classes.gridContainer}>
+        <Grid item md={12} className={`${classes.gridItem}`}>
+          <img
+            src='/images/bull2.png'
+            alt='sbsteakhouse'
+            className={classes.logo}
+          ></img>
+        </Grid>
+        <Grid item md={12} className={`${classes.gridItem}`}>
+          <Typography
+            variant='h3'
+            className={`${classes.Typo} ${classes.Typo1} `}
+          >
+            {item.typo1}
+          </Typography>
+        </Grid>
+        <Grid item md={12} className={`${classes.gridItem}`}>
+          <Typography
+            variant='subtitle1'
+            className={`${classes.Typo} ${classes.Typo2}`}
+          >
+            {item.typo2}
+          </Typography>
+        </Grid>
+
+        <Grid item md={12} className={`${classes.gridItem}`}>
+          <Button variant='outlined' style={{ border: 'none' }}>
+            <a
+              href='#!'
+              rel='noreferrer noopener'
+              className={`${classes.Typo} ${classes.btn} `}
+            >
+              {item.button}
+            </a>
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
