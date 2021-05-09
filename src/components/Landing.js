cfbf95fbde4c286200eb71601bad9d6c23957ee0
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-05-09 11:38:56 +0300

    edited height of landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxHeight: '95vh',
+    maxHeight: '100vh',
     objectFit: 'cover',
   },
   gridItem: {
