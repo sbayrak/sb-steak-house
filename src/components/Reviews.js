83e0b7a8ccdb03ca5685b60d56c51e3b6c2ec79d
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-05-10 17:01:39 +0300

    Implemented new carousel to Review section

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Box, Container, Grid, Typography } from '@material-ui/core';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+import { Grid, Typography, Container, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import steak4 from '../images/steak4.png';
 import RateReviewIcon from '@material-ui/icons/RateReview';
+
+var items = [
+  {
+    name: 'Suat Bayrak',
+    description:
+      'SB Steakhouse is a great place to spend time with your family. The service is also great!',
+  },
+  {
+    name: 'John Connor',
+    description:
+      'The best steak I have ever had!. Also the prices are reasonable, service is fast and good',
+  },
+  {
+    name: 'Kyle Reese',
+    description:
+      'I really enjoyed the steak. The smell, juicy and delicious...  ',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -62,69 +82,60 @@ const useStyles = makeStyles((theme) => ({
       fontSize: '12px',
     },
   },
+  carousel: {
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+  },
 }));
 
-export default function Reviews(props) {
+const Reviews = () => {
   const classes = useStyles();
-  var items = [
-    {
-      name: 'Suat Bayrak',
-      description:
-        'SB Steakhouse is a great place to spend time with your family. The service is also great!',
-    },
-    {
-      name: 'John Connor',
-      description:
-        'The best steak I have ever had!. Also the prices are reasonable, service is fast and good',
-    },
-    {
-      name: 'Kyle Reese',
-      description:
-        'I really enjoyed the steak. The smell, juicy and delicious...  ',
-    },
-  ];
-
   return (
     <Box component='div' className={classes.root}>
-      <Carousel indicators={false}>
-        {items.map((item, i) => (
-          <Item key={i} item={item} />
+      <Carousel
+        autoPlay
+        emulateTouch={true}
+        infiniteLoop
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        showArrows={false}
+        className={classes.carousel}
+      >
+        {items.map((item, index) => (
+          <Container maxWidth='sm' className={classes.container} key={index}>
+            <Grid container>
+              <Grid item md={12} className={classes.gridItem} xs={12}>
+                <RateReviewIcon
+                  fontSize='large'
+                  className={classes.reviewIcon}
+                ></RateReviewIcon>
+              </Grid>
+              <Grid item md={12} className={classes.gridItem} xs={12}>
+                <Typography
+                  variant='h3'
+                  className={`${classes.Typo} ${classes.Typo1}`}
+                  gutterBottom
+                >
+                  {item.name}
+                </Typography>
+              </Grid>
+              <Grid item md={12} className={classes.gridItem} xs={12}>
+                <Typography
+                  variant='body1'
+                  className={`${classes.Typo} ${classes.Typo2}`}
+                  gutterBottom
+                >
+                  {item.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
         ))}
       </Carousel>
     </Box>
   );
-}
+};
 
-function Item({ item }) {
-  const classes = useStyles();
-  return (
-    <Container maxWidth='sm' className={classes.container}>
-      <Grid container>
-        <Grid item md={12} className={classes.gridItem} xs={12}>
-          <RateReviewIcon
-            fontSize='large'
-            className={classes.reviewIcon}
-          ></RateReviewIcon>
-        </Grid>
-        <Grid item md={12} className={classes.gridItem} xs={12}>
-          <Typography
-            variant='h3'
-            className={`${classes.Typo} ${classes.Typo1}`}
-            gutterBottom
-          >
-            {item.name}
-          </Typography>
-        </Grid>
-        <Grid item md={12} className={classes.gridItem} xs={12}>
-          <Typography
-            variant='body1'
-            className={`${classes.Typo} ${classes.Typo2}`}
-            gutterBottom
-          >
-            {item.description}
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
-  );
-}
+export default Reviews;
