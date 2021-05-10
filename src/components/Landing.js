c7d381d6897b460714eb2b5fcd6f61e1672b1fff
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-05-10 14:27:27 +0300

    Added react-responsive-carousel and edited

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,208 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
+// import React from 'react';
+// import Carousel from 'react-material-ui-carousel';
+// import { Button, Grid, Typography } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Fade from 'react-reveal/Fade';
+// import { useState } from 'react';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     maxHeight: '100vh',
+//     objectFit: 'cover',
+//   },
+//   gridItem: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   gridContainer: {
+//     position: 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%,-50%)',
+//     textAlign: 'center',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   logo: {
+//     borderRadius: '20px',
+//     width: '250px',
+//     marginRight: theme.spacing(2),
+//     opacity: '0.8',
+//     marginBottom: theme.spacing(4),
+//     [theme.breakpoints.down('sm')]: {
+//       width: '175px',
+//     },
+//     [theme.breakpoints.down('xs')]: {
+//       width: '100px',
+//     },
+//   },
+//   Typo: {
+//     color: '#fff',
+//   },
+//   Typo1: {
+//     width: '100%',
+//     fontFamily: 'Quicksand, sans-serif',
+//     fontWeight: theme.typography.fontWeightBold,
+//     paddingBottom: theme.spacing(2),
+//     color: theme.palette.grey[300],
+//     letterSpacing: '3px',
+//     [theme.breakpoints.down('sm')]: {
+//       fontSize: '60px',
+//     },
+//     [theme.breakpoints.down('xs')]: {
+//       fontSize: '20px',
+//     },
+//   },
+//   Typo2: {
+//     width: '100%',
+//     paddingBottom: theme.spacing(7),
+//     color: theme.palette.grey[300],
+//     fontWeight: theme.typography.fontWeightMedium,
+//     fontSize: '16px',
+//     [theme.breakpoints.down('sm')]: {
+//       fontSize: '16px',
+//     },
+//     [theme.breakpoints.down('xs')]: {
+//       fontSize: '12px',
+//       width: '60%',
+//       margin: '0 auto',
+//     },
+//   },
+//   btn: {
+//     border: '1px solid #ff3c3c',
+//     padding: theme.spacing(1),
+//     paddingRight: theme.spacing(6),
+//     paddingLeft: theme.spacing(6),
+//     fontSize: '16px',
+//     transition: '0.3s ease',
+//     textDecoration: 'none',
+//     borderRadius: '5px',
+//     '&:hover': {
+//       border: `1px solid ${theme.palette.grey[200]}`,
+//     },
+//   },
+//   img: {
+//     height: '100vh',
+//     width: '100%',
+//     [theme.breakpoints.down('sm')]: {
+//       width: '100em',
+//     },
+//   },
+// }));
+
+// function Landing(props) {
+//   var items = [
+//     {
+//       img: '/images/steak1.png',
+//       typo1: 'STEAK, RIB-EYE, T-BONE',
+//       typo2:
+//         'Our meats are getting cooked for 12 hours with walnut and oak woods to get that flavour.',
+//       button: 'MORE',
+//     },
+//     {
+//       img: '/images/steak2.png',
+//       typo1: 'WOOD, FIRE, CHARCOAL',
+//       typo2:
+//         'In BAYRAK Steakhouse, you will not feel a different taste than the BBQ you make at home  ',
+//       button: 'CHEFS',
+//     },
+//     {
+//       img: '/images/steak3.png',
+//       typo1: 'QUALITY, SATISFACTION, TASTE',
+//       typo2:
+//         'Since 1995, the good quality meal comes from good quality meat. That`s why we are so careful about choosing the right meat.',
+//       button: 'MENU',
+//     },
+//   ];
+
+//   return (
+//     <Carousel
+//       interval='4000'
+//       indicators='true'
+//       navButtonsAlwaysVisible='true'
+//       timeout={{ appear: 0, enter: 0, exit: 0 }}
+//     >
+//       {items.map((item, i) => (
+//         <Item key={i} item={item} />
+//       ))}
+//     </Carousel>
+//   );
+// }
+
+// function Item({ item }) {
+//   const classes = useStyles();
+//   const [loading, setLoading] = useState(false);
+
+//   const style = loading ? {} : { visibility: 'hidden' };
+
+//   return (
+//     <div className={classes.root}>
+//       <img
+//         src={item.img}
+//         alt={item.img}
+//         className={classes.img}
+//         style={style}
+//         onLoad={() => setLoading(true)}
+//       ></img>
+
+//       <Grid container className={classes.gridContainer}>
+//         <Fade top>
+//           <Grid item md={12} className={`${classes.gridItem}`}>
+//             <img
+//               src='/images/bull2.png'
+//               alt='sbsteakhouse'
+//               className={classes.logo}
+//             ></img>
+//           </Grid>
+//         </Fade>
+//         <Grid item md={12} className={`${classes.gridItem}`}>
+//           <Fade bottom>
+//             <Typography
+//               variant='h3'
+//               className={`${classes.Typo} ${classes.Typo1} `}
+//             >
+//               {item.typo1}
+//             </Typography>
+//           </Fade>
+//         </Grid>
+//         <Grid item md={12} className={`${classes.gridItem}`}>
+//           <Fade bottom>
+//             <Typography
+//               variant='subtitle1'
+//               className={`${classes.Typo} ${classes.Typo2}`}
+//             >
+//               {item.typo2}
+//             </Typography>
+//           </Fade>
+//         </Grid>
+//         <Grid item md={12} className={`${classes.gridItem}`}>
+//           <Fade bottom>
+//             <Button variant='outlined' style={{ border: 'none' }}>
+//               <a
+//                 href='#!'
+//                 rel='noreferrer noopener'
+//                 className={`${classes.Typo} ${classes.btn} `}
+//               >
+//                 {item.button}
+//               </a>
+//             </Button>
+//           </Fade>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// }
+
+// export default Landing;
+
+import React, { Fragment } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 import { Button, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Fade from 'react-reveal/Fade';
-import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +226,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     borderRadius: '20px',
-    width: '250px',
+    width: '150px',
     marginRight: theme.spacing(2),
     opacity: '0.8',
     marginBottom: theme.spacing(4),
@@ -35,7 +234,7 @@ const useStyles = makeStyles((theme) => ({
       width: '175px',
     },
     [theme.breakpoints.down('xs')]: {
-      width: '100px',
+      width: '50px',
     },
   },
   Typo: {
@@ -85,15 +284,31 @@ const useStyles = makeStyles((theme) => ({
   },
   img: {
     height: '100vh',
-    width: '100%',
+  },
+  carouselDivDesktop: {
+    height: '100vh',
+  },
+  carouselDivMobile: {
+    height: '100vh',
+  },
+  desktopDiv: {
+    display: 'inline',
     [theme.breakpoints.down('sm')]: {
-      width: '100em',
+      display: 'none',
+    },
+  },
+  mobileDiv: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'inline',
     },
   },
 }));
 
-function Landing(props) {
-  var items = [
+const Landing = () => {
+  const classes = useStyles();
+
+  var itemsDesktop = [
     {
       img: '/images/steak1.png',
       typo1: 'STEAK, RIB-EYE, T-BONE',
@@ -117,68 +332,60 @@ function Landing(props) {
     },
   ];
 
-  return (
-    <Carousel
-      interval='4000'
-      indicators='true'
-      navButtonsAlwaysVisible='true'
-      timeout={{ appear: 0, enter: 0, exit: 0 }}
-    >
-      {items.map((item, i) => (
-        <Item key={i} item={item} />
-      ))}
-    </Carousel>
-  );
-}
+  var itemsMobile = [
+    {
+      img: '/images/steak1mobile.png',
+      typo1: 'STEAK, RIB-EYE, T-BONE',
+      typo2:
+        'Our meats are getting cooked for 12 hours with walnut and oak woods to get that flavour.',
+      button: 'MORE',
+    },
+    {
+      img: '/images/steak2mobile.png',
+      typo1: 'WOOD, FIRE, CHARCOAL',
+      typo2:
+        'In BAYRAK Steakhouse, you will not feel a different taste than the BBQ you make at home  ',
+      button: 'CHEFS',
+    },
+    {
+      img: '/images/steak3mobile.png',
+      typo1: 'QUALITY, SATISFACTION, TASTE',
+      typo2:
+        'Since 1995, the good quality meal comes from good quality meat. That`s why we are so careful about choosing the right meat.',
+      button: 'MENU',
+    },
+  ];
 
-function Item({ item }) {
-  const classes = useStyles();
-  const [loading, setLoading] = useState(false);
-
-  const style = loading ? {} : { visibility: 'hidden' };
-
-  return (
-    <div className={classes.root}>
-      <img
-        src={item.img}
-        alt={item.img}
-        className={classes.img}
-        style={style}
-        onLoad={() => setLoading(true)}
-      ></img>
-
-      <Grid container className={classes.gridContainer}>
-        <Fade top>
-          <Grid item md={12} className={`${classes.gridItem}`}>
+  const desktop = itemsDesktop.map((item, index) => (
+    <div className={classes.carouselDivDesktop}>
+      <Fragment>
+        <img src={item.img} alt='sbsteakhouse' className={classes.img} />
+        <Grid container className={classes.gridContainer}>
+          <Grid item md={2} className={`${classes.gridItem}`}>
             <img
               src='/images/bull2.png'
-              alt='sbsteakhouse'
+              alt='sb_steakhouse'
               className={classes.logo}
-            ></img>
+            />
           </Grid>
-        </Fade>
-        <Grid item md={12} className={`${classes.gridItem}`}>
-          <Fade bottom>
+
+          <Grid item md={12} className={`${classes.gridItem}`}>
             <Typography
               variant='h3'
               className={`${classes.Typo} ${classes.Typo1} `}
             >
               {item.typo1}
             </Typography>
-          </Fade>
-        </Grid>
-        <Grid item md={12} className={`${classes.gridItem}`}>
-          <Fade bottom>
+          </Grid>
+          <Grid item md={12} className={`${classes.gridItem}`}>
             <Typography
               variant='subtitle1'
               className={`${classes.Typo} ${classes.Typo2}`}
             >
               {item.typo2}
             </Typography>
-          </Fade>
-        </Grid>
-        <Grid item md={12} className={`${classes.gridItem}`}>
-          <Fade bottom>
+          </Grid>
+          <Grid item md={12} className={`${classes.gridItem}`}>
             <Button variant='outlined' style={{ border: 'none' }}>
               <a
                 href='#!'
@@ -188,11 +395,81 @@ function Item({ item }) {
                 {item.button}
               </a>
             </Button>
-          </Fade>
+          </Grid>
         </Grid>
-      </Grid>
+      </Fragment>
     </div>
+  ));
+
+  const mobile = itemsMobile.map((item, index) => (
+    <div className={classes.carouselDivMobile}>
+      <Fragment>
+        <img src={item.img} alt='sbsteakhouse' className={classes.img} />
+        <Grid container className={classes.gridContainer}>
+          <Grid item md={12} className={`${classes.gridItem}`}>
+            <img
+              src='/images/bull2mobile.png'
+              alt='sb_steakhouse'
+              className={classes.logo}
+              width='100px'
+            />
+          </Grid>
+
+          <Grid item md={12} className={`${classes.gridItem}`}>
+            <Typography
+              variant='h3'
+              className={`${classes.Typo} ${classes.Typo1} `}
+            >
+              {item.typo1}
+            </Typography>
+          </Grid>
+          <Grid item md={12} className={`${classes.gridItem}`}>
+            <Typography
+              variant='subtitle1'
+              className={`${classes.Typo} ${classes.Typo2}`}
+            >
+              {item.typo2}
+            </Typography>
+          </Grid>
+          <Grid item md={12} className={`${classes.gridItem}`}>
+            <Button variant='outlined' style={{ border: 'none' }}>
+              <a
+                href='#!'
+                rel='noreferrer noopener'
+                className={`${classes.Typo} ${classes.btn} `}
+              >
+                {item.button}
+              </a>
+            </Button>
+          </Grid>
+        </Grid>
+      </Fragment>
+    </div>
+  ));
+
+  return (
+    <Fragment>
+      <Carousel
+        autoPlay
+        emulateTouch={true}
+        infiniteLoop
+        showThumbs={false}
+        className={classes.mobileDiv}
+      >
+        {mobile}
+      </Carousel>
+
+      <Carousel
+        autoPlay
+        emulateTouch={true}
+        infiniteLoop
+        showThumbs={false}
+        className={classes.desktopDiv}
+      >
+        {desktop}
+      </Carousel>
+    </Fragment>
   );
-}
+};
 
 export default Landing;
